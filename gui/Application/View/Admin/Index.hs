ff692b6d02f4447f8debd8042568dd685c6b0705
Pelayo Maojo <pelayo.mao@gmail.com>
2025-08-17 21:12:21 +0200

    Add admin CLI runner

--- gui/Application/View/Admin/Index.hs
+++ gui/Application/View/Admin/Index.hs
@@ -8,7 +8,8 @@ instance View IndexView where
     html IndexView = layout [hsx|
         <h1>Admin Dashboard</h1>
         <ul>
-            <li><a href="/Cli">CLI</a></li>
+            <li><a href="/Admin/RunCli">Run CLI Command</a></li>
+            <li><a href="/CliRuns">CLI Runs</a></li>
             <li><a href="/Users">Users</a></li>
             <li><a href="/Posts">Posts</a></li>
         </ul>
